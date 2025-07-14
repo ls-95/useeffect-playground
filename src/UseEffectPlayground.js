8c2e26cd06b2df6e5bda732f05d757aaca5a62ea
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-07-14 11:06:28 +0200

    Add effects to counting

--- src/UseEffectPlayground.js
+++ src/UseEffectPlayground.js
@@ -1,8 +1,28 @@
 import "./UseEffectPlayground.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 
 export default function UseEffectPlayground() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("This runs on every render, Current count: ", count);
+  });
+
+  useEffect(() => {
+    console.log(
+      "This runs ONCE when the component first loads because of the []."
+    );
+    console.log("This is a good for initial data fetching.");
+  }, []);
+
+  useEffect(() => {
+    console.log("The count has changed. New value: ", count);
+    if (count === 0) {
+      document.title = "useEffect Playground";
+    } else {
+      document.title = `Count: ${count}`;
+    }
+  }, [count]);
 
   return (
     <div className="container">
@@ -18,6 +38,7 @@ export default function UseEffectPlayground() {
               <button onClick={() => setCount(count - 1)}>-1</button>
               <button onClick={() => setCount(0)}>Reset</button>
             </div>
+            <p>useEffect can be seen in the console</p>
           </div>
         </div>
       </div>
